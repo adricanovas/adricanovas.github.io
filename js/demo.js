f365da4139d6ad6bf9970661ea0610842c400d90
Adri Canovas <adrian.canovas1@um.es>
2022-04-04 14:34:52 +0200

    Cambio de color al refrescar la página

--- js/demo.js
+++ js/demo.js
@@ -5,24 +5,12 @@
 
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: "#" + randomColor,
+    lineColor: "#" + randomColor
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
 
-
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
